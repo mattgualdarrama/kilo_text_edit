12c83a6a28766d3fb21a55203225b34d64d34193
Matt G <mattgualdarrama@gmail.com>
2020-01-10 17:31:10 -0500

    fixed cursor rendor issue

--- kilo.c
+++ kilo.c
@@ -202,10 +202,10 @@ void editorRefreshScreen() {
 	editorDrawRows(&ab);
 
 	char buf[32];
-	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy +1, E.cx + 1);	abAppend(&ab, "\x1b[H", 3);
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy +1, E.cx + 1);	
 	abAppend(&ab, buf, strlen(buf));
 	
-	abAppend(&ab, "x1b[?25h", 6);
+	abAppend(&ab, "\x1b[?25h", 6);
 
 	write(STDOUT_FILENO, ab.b, ab.len);
 	abFree(&ab);
