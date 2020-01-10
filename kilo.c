16130967d0544750f94853bd7cb3a5db52aab22f
Matt G <mattgualdarrama@gmail.com>
2020-01-10 17:25:03 -0500

    handle home, end, delete keys

--- kilo.c
+++ kilo.c
@@ -32,6 +32,9 @@ enum editorKey {
 	ARROW_RIGHT,
 	ARROW_UP,
 	ARROW_DOWN,
+	DEL_KEY,
+	HOME_KEY,
+	END_KEY,
 	PAGE_UP,
 	PAGE_DOWN
 };
@@ -75,15 +78,22 @@ int editorReadKey() {
   }
   if (c == '\x1b') {
     char seq[3];
+
     if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
     if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
     if (seq[0] == '[') {
       if (seq[1] >= '0' && seq[1] <= '9') {
         if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
         if (seq[2] == '~') {
           switch (seq[1]) {
+			case '1': return HOME_KEY;
+			case '3': return DEL_KEY;
+			case '4': return END_KEY;
             case '5': return PAGE_UP;
             case '6': return PAGE_DOWN;
+			case '7': return HOME_KEY;
+			case '8': return END_KEY;
           }
         }
       } else {
@@ -92,9 +102,16 @@ int editorReadKey() {
           case 'B': return ARROW_DOWN;
           case 'C': return ARROW_RIGHT;
           case 'D': return ARROW_LEFT;
+		  case 'H': return HOME_KEY;
+		  case 'F': return END_KEY;
         }
       }
-    }
+    } else if (seq[0] == 'O') {
+		switch (seq[1]) {
+			case 'H': return HOME_KEY;
+			case 'F': return END_KEY;
+		}
+	}
     return '\x1b';
   } else {
     return c;
@@ -229,6 +246,14 @@ void editorProcessKeypress() {
 			write(STDOUT_FILENO, "\x1b[2j", 4);
 			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
+			break;
+		
+		case HOME_KEY:
+			E.cx = 0;
+			break;
+		
+		case END_KEY:
+			E.cx = E.screencols -1;
 			break;
 		
 		case PAGE_UP:
