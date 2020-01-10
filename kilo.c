63142d4ae8727c2e7b164a4294977170a5691bc1
Matt G <mattgualdarrama@gmail.com>
2020-01-10 16:59:06 -0500

    handle arrow key presses

--- kilo.c
+++ kilo.c
@@ -12,6 +12,7 @@
 //data
 
 struct editorConfig {
+	int cx, cy;
 	int screenrows;
 	int screencols;
 	struct termios orig_termios;
@@ -25,6 +26,13 @@ struct editorConfig E;
 #define KILO_VERSION "0.0.1"
 
 #define CTRL_KEY(k) ((k) & 0x1f)
+
+enum editorKey {
+	ARROW_LEFT = 1000,
+	ARROW_RIGHT,
+	ARROW_UP,
+	ARROW_DOWN
+};
 
 // data
 
@@ -57,13 +65,31 @@ void enableRawMode() {
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-char editorReadKey() {
+int editorReadKey() {
 	int nread;
 	char c;
 	while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
 		if (nread == -1 && errno != EAGAIN) die("read");
 	}
-	return c;
+
+	if (c == '\x1b') {
+		char seq[3];
+
+		if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+		if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+
+		if (seq[0] == '[') {
+			switch (seq[1]) {
+				case 'A': return ARROW_UP;
+				case 'B': return ARROW_DOWN;
+				case 'C': return ARROW_RIGHT;
+				case 'D': return ARROW_LEFT;
+			}
+		}
+		return '\x1b';
+	} else {
+		return c;
+	}
 }
 
 int getCursorPosition(int *rows, int *cols) {
@@ -148,8 +174,11 @@ void editorRefreshScreen() {
 	abAppend(&ab, "\x1b[H", 3);
 
 	editorDrawRows(&ab);
-\
-	abAppend(&ab, "\x1b[H", 3);
+
+	char buf[32];
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy +1, E.cx + 1);	abAppend(&ab, "\x1b[H", 3);
+	abAppend(&ab, buf, strlen(buf));
+	
 	abAppend(&ab, "x1b[?25h", 6);
 
 	write(STDOUT_FILENO, ab.b, ab.len);
@@ -158,8 +187,25 @@ void editorRefreshScreen() {
 
 // input
 
+void editorMoveCursor(int key) {
+	switch (key) {
+		case ARROW_LEFT:
+			E.cx--;
+			break;
+		case ARROW_RIGHT:
+			E.cx++;
+			break;
+		case ARROW_UP:
+			E.cy--;
+			break;
+		case ARROW_DOWN:
+			E.cy++;
+			break;
+	}
+}
+
 void editorProcessKeypress() {
-	char c = editorReadKey();
+	int c = editorReadKey();
 
 	switch (c) {
 		case CTRL_KEY('q'):
@@ -167,12 +213,21 @@ void editorProcessKeypress() {
 			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
 			break;
+		
+		case ARROW_UP:
+		case ARROW_DOWN:
+		case ARROW_LEFT:
+		case ARROW_RIGHT:
+			editorMoveCursor(c);
+			break;
 	}
 }
 
 // init
 
 void initEditor() {
+	E.cx = 0;
+	E.cy = 0;
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
